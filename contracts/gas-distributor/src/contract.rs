a483f01e9657afb84cb479dd54c6b8d21b687f61
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2024-10-30 16:53:08 +0100

    Remove unused import

--- contracts/gas-distributor/src/contract.rs
+++ contracts/gas-distributor/src/contract.rs
@@ -1,6 +1,6 @@
 use cosmwasm_std::{
     attr, entry_point, to_json_binary, Addr, BankMsg, Binary, Coin, CosmosMsg, Deps, DepsMut, Env,
-    Event, MessageInfo, Order, Response, Uint128,
+    Event, MessageInfo, Order, Response,
 };
 use drop_helper_contracts_base::{
     error::gas_distributor::ContractError,

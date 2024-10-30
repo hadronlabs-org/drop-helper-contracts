ca3bf6de6244f2b5621da1ee8c18a09598dfe342
Vladislav Vasilev <81014877+faust403@users.noreply.github.com>
2024-10-30 19:56:03 +0100

    Add instantiate unit tests

--- contracts/gas-distributor/src/tests.rs
+++ contracts/gas-distributor/src/tests.rs
@@ -11,10 +11,8 @@ use drop_helper_contracts_base::{
 use drop_helper_contracts_helpers::testing::mock_dependencies;
 
 #[test]
-fn test_instantiate() {
+fn test_instantiate_general() {
     let mut deps = mock_dependencies(&[]);
-    let deps_mut = deps.as_mut();
-    let _ = cw_ownable::initialize_owner(deps_mut.storage, deps_mut.api, Some("owner")).unwrap();
     let response = contract::instantiate(
         deps.as_mut().into_empty(),
         mock_env(),
@@ -43,10 +41,14 @@ fn test_instantiate() {
 
     assert_eq!(
         response,
-        Response::new().add_event(Event::new("instantiate").add_attributes(vec![
-            attr("add_target_balance", "address1"),
-            attr("add_target_balance", "address2")
-        ]))
+        Response::new().add_event(
+            Event::new("crates.io:drop-helper__drop-gas-distributor-instantiate").add_attributes(
+                vec![
+                    attr("add-target-balance", "address1"),
+                    attr("add-target-balance", "address2")
+                ]
+            )
+        )
     );
 
     let res: Vec<TargetBalance> = from_json(
@@ -78,4 +80,96 @@ fn test_instantiate() {
             }
         ]
     );
+}
+
+#[test]
+fn test_instantiate_custom_owner() {
+    let mut deps = mock_dependencies(&[]);
+    let response = contract::instantiate(
+        deps.as_mut().into_empty(),
+        mock_env(),
+        mock_info("sender", &[]),
+        InstantiateMsg {
+            owner: Some(Addr::unchecked("owner")),
+            initial_target_balances: vec![
+                TargetBalance {
+                    address: Addr::unchecked("address1"),
+                    update_options: TargetBalanceUpdateParams {
+                        target_balance: Uint128::from(123_u64),
+                        update_value: Some(Uint128::from(2000_u64)),
+                    },
+                },
+                TargetBalance {
+                    address: Addr::unchecked("address2"),
+                    update_options: TargetBalanceUpdateParams {
+                        target_balance: Uint128::from(321_u64),
+                        update_value: Some(Uint128::from(1000_u64)),
+                    },
+                },
+            ],
+        },
+    )
+    .unwrap();
+
+    assert_eq!(
+        response,
+        Response::new().add_event(
+            Event::new("crates.io:drop-helper__drop-gas-distributor-instantiate").add_attributes(
+                vec![
+                    attr("add-target-balance", "address1"),
+                    attr("add-target-balance", "address2")
+                ]
+            )
+        )
+    );
+    let owner: String =
+        from_json(query(deps.as_ref().into_empty(), mock_env(), QueryMsg::Owner {}).unwrap())
+            .unwrap();
+    assert_eq!(owner, "owner");
+}
+
+#[test]
+fn test_instantiate_sender_owner() {
+    let mut deps = mock_dependencies(&[]);
+    let response = contract::instantiate(
+        deps.as_mut().into_empty(),
+        mock_env(),
+        mock_info("sender", &[]),
+        InstantiateMsg {
+            owner: None,
+            initial_target_balances: vec![
+                TargetBalance {
+                    address: Addr::unchecked("address1"),
+                    update_options: TargetBalanceUpdateParams {
+                        target_balance: Uint128::from(123_u64),
+                        update_value: Some(Uint128::from(2000_u64)),
+                    },
+                },
+                TargetBalance {
+                    address: Addr::unchecked("address2"),
+                    update_options: TargetBalanceUpdateParams {
+                        target_balance: Uint128::from(321_u64),
+                        update_value: Some(Uint128::from(1000_u64)),
+                    },
+                },
+            ],
+        },
+    )
+    .unwrap();
+
+    assert_eq!(
+        response,
+        Response::new().add_event(
+            Event::new("crates.io:drop-helper__drop-gas-distributor-instantiate").add_attributes(
+                vec![
+                    attr("add-target-balance", "address1"),
+                    attr("add-target-balance", "address2")
+                ]
+            )
+        )
+    );
+    let owner: String =
+        from_json(query(deps.as_ref().into_empty(), mock_env(), QueryMsg::Owner {}).unwrap())
+            .unwrap();
+    assert_eq!(owner, "sender");
 }
